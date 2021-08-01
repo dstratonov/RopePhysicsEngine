4663fecfac2175ab59a8eb9feacb0c214572cd39
dstratonov <stratonov1234@gmail.com>
2021-08-01 13:00:42 +0300

    added initial project

---
+++ PhysicsRopeEngine/cmake-build-debug/CMakeFiles/PhysicsRopeEngine.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PhysicsRopeEngine.dir/main.cpp.obj"
+  "PhysicsRopeEngine.exe"
+  "PhysicsRopeEngine.exe.manifest"
+  "PhysicsRopeEngine.pdb"
+  "libPhysicsRopeEngine.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PhysicsRopeEngine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
